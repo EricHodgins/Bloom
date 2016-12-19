50593f4b0b9df4a7c5229709f8f34df53cf01506
Eric Hodgins <hodgins.e@gmail.com>
2016-12-19 00:53:58 -0500

    feat: added lineSeparator animation to create workout controller.

--- Bloom/CreateWorkoutController.swift
+++ Bloom/CreateWorkoutController.swift
@@ -10,26 +10,94 @@ import UIKit
 
 class CreateWorkoutController: UIViewController {
 
+    @IBOutlet weak var workoutNameTextfield: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let lineSeparator: UIView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        workoutNameTextfield.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func cancelPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func savePressed(_ sender: Any) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func addExcercisePressed(_ sender: Any) {
+        
     }
-    */
-
+    
+    override func viewDidLayoutSubviews() {
+        setupLineSeparator()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animateLineSeparator()
+    }
+    
+    func setupLineSeparator() {
+        lineSeparator.translatesAutoresizingMaskIntoConstraints = false
+        lineSeparator.backgroundColor = UIColor.white
+        view.addSubview(lineSeparator)
+        
+        NSLayoutConstraint.activate([
+            lineSeparator.topAnchor.constraint(equalTo: workoutNameTextfield.bottomAnchor, constant: 10),
+            lineSeparator.centerXAnchor.constraint(equalTo: workoutNameTextfield.centerXAnchor),
+            lineSeparator.widthAnchor.constraint(equalToConstant: 1),
+            lineSeparator.heightAnchor.constraint(equalToConstant: 1)
+        ])
+    }
+    
 }
+
+extension CreateWorkoutController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.isFirstResponder {
+            textField.resignFirstResponder()
+        }
+        
+        return true
+    }
+}
+
+// MARK: - Animations
+extension CreateWorkoutController {
+    func animateLineSeparator() {
+        UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+            let scaleX = CGFloat(self.view.bounds.size.width * 0.9)
+            self.lineSeparator.transform = CGAffineTransform(scaleX: scaleX, y: 2)
+        }, completion: nil)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
