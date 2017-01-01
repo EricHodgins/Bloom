168823dc508d9fc1fcdfda3841cdae9fc048a1bc
Eric Hodgins <hodgins.e@gmail.com>
2017-01-01 13:17:18 -0500

    feat: Added the timer for LiveWorkoutController to track the duration of the workout.

--- Bloom/LiveWorkoutController.swift
+++ Bloom/LiveWorkoutController.swift
@@ -15,13 +15,14 @@ class LiveWorkoutController: UIViewController {
     @IBOutlet weak var heartBeatView: UIView!
     @IBOutlet weak var currentExcerciseLabel: UILabel!
 
-    
+    var startTime: TimeInterval!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        startTime = Date.timeIntervalSinceReferenceDate
+        Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(LiveWorkoutController.startTimer), userInfo: nil, repeats: true)
     }
     
     @IBAction func nextExcerciseButtonPressed(_ sender: Any) {
@@ -31,3 +32,69 @@ class LiveWorkoutController: UIViewController {
     }
     
 }
+
+extension LiveWorkoutController {
+    func startTimer() {
+        let finish = Date.timeIntervalSinceReferenceDate
+        var diff = finish - startTime
+        
+        let hours = Int16(diff / 3600)
+        
+        diff -= TimeInterval(hours * 3600)
+        
+        let minutes = Int8(diff / 60)
+        
+        diff -= TimeInterval(minutes * 60)
+        
+        let seconds = Int8(diff)
+        
+        let hoursFormatted = String(format: "%02d", hours)
+        let minutesFormatted = String(format: "%02d", minutes)
+        let secondsFormatted = String(format: "%02d", seconds)
+        
+        DispatchQueue.main.async {
+            self.workoutDurationLabel.text = "\(hoursFormatted):\(minutesFormatted):\(secondsFormatted)"
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Bloom/RetainCycles.playground/Contents.swift
+++ Bloom/RetainCycles.playground/Contents.swift
@@ -7,7 +7,7 @@ import PlaygroundSupport
 
 
 func delay(seconds: Int, completionHandler: @escaping (() -> Void)) {
-    let delayInMilliSeconds = DispatchTime.now() + DispatchTimeInterval.milliseconds(seconds)
+    let delayInMilliSeconds = DispatchTime.now() + DispatchTimeInterval.seconds(seconds)
     DispatchQueue.main.asyncAfter(deadline: delayInMilliSeconds, execute: completionHandler)
 }
 
@@ -41,19 +41,68 @@ class AssembleView  {
     }
 }
 
-var workout: StartWorkout? = StartWorkout()
-var assembleView: AssembleView? = AssembleView()
-assembleView?.delegate = workout
-assembleView?.beginCountDown()
+//var workout: StartWorkout? = StartWorkout()
+//var assembleView: AssembleView? = AssembleView()
+//assembleView?.delegate = workout
+//assembleView?.beginCountDown()
+//
+//
+//workout = nil
+//assembleView = nil
+
+class StopWatch {
+    var startTime = TimeInterval()
+    
+    init() {
+        Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(StopWatch.updateTime), userInfo: nil, repeats: true)
+        startTime = Date.timeIntervalSinceReferenceDate
+    }
+    
+    @objc func updateTime() {
+        let finish = Date.timeIntervalSinceReferenceDate
+        var diff = finish - startTime
+        
+        let hours = Int16(diff / 3600)
+        
+        diff -= TimeInterval(hours * 3600)
+        
+        let minutes = Int8(diff / 60)
+        
+        diff -= TimeInterval(minutes * 60)
+        
+        let seconds = Int8(diff)
+        
+        let hoursFormatted = String(format: "%02d", hours)
+        let minutesFormatted = String(format: "%02d", minutes)
+        let secondsFormatted = String(format: "%02d", seconds)
+        
+        print("\(hoursFormatted):\(minutesFormatted):\(secondsFormatted)")//:\(minutes):\(seconds)")
+    }
+}
 
 
-workout = nil
-assembleView = nil
+var start: TimeInterval = Date.timeIntervalSinceReferenceDate
+
+delay(seconds: 3) {
+    let finish = Date.timeIntervalSinceReferenceDate + 3660
+    var diff = finish - start
+    print("\(start) - \(finish))")
+    
+    let hours = Int16(diff / 3600)
+    
+    diff -= TimeInterval(hours * 3600)
+    
+    let minutes = Int8(diff / 60)
+    
+    diff -= TimeInterval(minutes * 60)
+    
+    let seconds = Int8(diff)
+    
+    print("\(hours):\(minutes):\(seconds)")//:\(minutes):\(seconds)")
+}
 
 
-
-
-
+let stopWatch = StopWatch()
 
 
 
