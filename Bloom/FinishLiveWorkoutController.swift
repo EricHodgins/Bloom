f58db7a82274a85446e60ad13bcfd2600f76c882
Eric Hodgins <hodgins.e@gmail.com>
2017-01-14 20:48:16 -0500

    feat: Added a MetalImageView. Trying out a cool sunbeam animation on the Finish Live Workout VC.

--- Bloom/FinishLiveWorkoutController.swift
+++ Bloom/FinishLiveWorkoutController.swift
@@ -7,13 +7,78 @@
 //
 
 import UIKit
+import MetalKit
 
 class FinishLiveWorkoutController: UIViewController {
+    
+    var timeTracker: Double = 0.0
+    var positionTracker: CGFloat = 0.0
+    var displayLink = CADisplayLink()
+    
+    lazy var metalView: MetalImageView = {
+        let view = MetalImageView(frame: self.view.bounds, device: nil)
+        return view
+    }()
+    
+    private lazy var beamFilter: CIFilter = {
+        let filter = CIFilter(name: "CISunbeamsGenerator")!
+        filter.setValue(100, forKeyPath: "inputSunRadius")
+        filter.setValue(10, forKey: "inputMaxStriationRadius")
+        filter.setValue(0.2, forKey: "inputStriationStrength")
+        
+        return filter
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.insertSubview(metalView, at: 0)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    @IBAction func testAnimation(_ sender: Any) {
+        startAnimatingSunbeam()
+    }
+    func startAnimatingSunbeam() {
+        displayLink = CADisplayLink(target: self, selector: #selector(FinishLiveWorkoutController.animate(displayLink:)))
+        displayLink.add(to: RunLoop.main, forMode: .defaultRunLoopMode)
+    }
+    
+    func animate(displayLink: CADisplayLink) {
+        timeTracker += 0.0001
+        positionTracker += 0.05
+        beamFilter.setValue(timeTracker, forKey: kCIInputTimeKey)
+        beamFilter.setValue(CIVector(x: positionTracker, y: positionTracker), forKeyPath: "inputCenter")
+        metalView.image = beamFilter.outputImage!
+        
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Bloom/MetalImageView.swift
@@ -0,0 +1,93 @@
+//
+//  MetalImageView.swift
+//  Bloom
+//
+//  Created by Eric Hodgins on 2017-01-14.
+//  Copyright © 2017 Eric Hodgins. All rights reserved.
+//
+
+import MetalKit
+
+class MetalImageView: MTKView {
+    var image: CIImage? {
+        didSet {
+            renderImage()
+        }
+    }
+    
+    lazy var ciContext: CIContext = {
+        return CIContext(mtlDevice: self.device!)
+    }()
+    
+    lazy var commandQueue: MTLCommandQueue = {
+        return self.device!.makeCommandQueue()
+    }()
+    
+    let colorSpace = CGColorSpaceCreateDeviceRGB()
+    
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame frameRect: CGRect, device: MTLDevice?) {
+        super.init(frame: frameRect, device: device ?? MTLCreateSystemDefaultDevice())
+        
+        if super.device == nil {
+            fatalError("Device not supported.")
+        }
+        
+        framebufferOnly = false // Allows to write to the View's texture.
+    }
+    
+    
+    func renderImage() {
+        guard let image = image,
+            let targetTexture = currentDrawable?.texture else { return }
+        
+        let bounds = CGRect(origin: .zero, size: drawableSize)
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        
+        ciContext.render(image, to: targetTexture, commandBuffer: commandBuffer, bounds: bounds, colorSpace: colorSpace)
+        
+        commandBuffer.present(currentDrawable!)
+        commandBuffer.commit()
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
