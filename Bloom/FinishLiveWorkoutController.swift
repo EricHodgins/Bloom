09a91ec064e4d6caeb0136c327b978fc77d460da
Eric Hodgins <hodgins.e@gmail.com>
2017-06-08 10:53:34 -0400

    feat: Added particle effect to FInishWorkoutController.

--- Bloom/FinishLiveWorkoutController.swift
+++ Bloom/FinishLiveWorkoutController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class FinishLiveWorkoutController: UIViewController {
 
+    @IBOutlet weak var finishWorkoutButton: GenericBloomButton!
     //var sunsetBackground: SunsetBackground!
     var workout: Workout!
     
@@ -22,6 +23,28 @@ class FinishLiveWorkoutController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        let rect = CGRect(x: 0.0, y: 0.0, width: view.bounds.width, height: view.bounds.height)
+        let emitter = CAEmitterLayer()
+        emitter.frame = rect
+        view.layer.insertSublayer(emitter, below: finishWorkoutButton.layer)
+        
+        emitter.emitterShape = kCAEmitterLayerLine
+        emitter.emitterPosition = CGPoint(x: rect.width/2, y: rect.height)
+        emitter.emitterSize = rect.size
+        
+        let emitterCell = CAEmitterCell()
+        emitterCell.contents = UIImage(named: "spark.png")?.cgImage
+        
+        emitterCell.birthRate = 10
+        emitterCell.lifetime = 50.0
+        emitter.emitterCells = [emitterCell]
+        emitterCell.yAcceleration = -5.0
+        emitterCell.scale = 0.07
+        emitterCell.scaleRange = 0.1
+        emitterCell.redRange = 0.3
+        emitterCell.greenRange = 0.9
+        emitterCell.blueRange  = 0.3
+        emitterCell.color = UIColor(red: 243/255, green: 23/255, blue: 0/255, alpha: 1.0).cgColor
     }
     
     @IBAction func finishWorkoutButtonPressed(_ sender: Any) {
