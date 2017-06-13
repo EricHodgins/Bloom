9487d97f3b871a349d2c4466cf1cac227d49e4f2
Eric Hodgins <hodgins.e@gmail.com>
2017-06-13 00:47:39 -0400

    feat: Added segue to show specific workout info.  Setup basic UI for ViewController.

--- Bloom/SummaryViewController.swift
+++ Bloom/SummaryViewController.swift
@@ -13,16 +13,18 @@ class SummaryViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var managedContext: NSManagedObjectContext!
-    var workouts: [NSDictionary] = []
+    var workoutTypes: [NSDictionary] = []
+    var workouts: [Workout] = []
+    var isAllWorkouts: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        fetch()
+        fetchWorkoutTypes()
     }
     
-    func fetch() {
+    func fetchWorkoutTypes() {
         let fetchRequest = NSFetchRequest<NSDictionary>(entityName: "WorkoutTemplate")
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: #keyPath(WorkoutTemplate.name), ascending: true)]
         fetchRequest.resultType = .dictionaryResultType
@@ -30,9 +32,20 @@ class SummaryViewController: UIViewController {
         fetchRequest.propertiesToFetch = ["name"]
         
         do {
-            workouts = try managedContext.fetch(fetchRequest)
+            workoutTypes = try managedContext.fetch(fetchRequest)
         } catch let error as NSError {
             print("Workout template fetch error: \(error.userInfo)")
+        }
+    }
+    
+    func fetchAllWorkouts() {
+        let fetchRequest = NSFetchRequest<Workout>(entityName: "Workout")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: #keyPath(Workout.name), ascending: true)]
+        
+        do {
+            workouts = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("All workout fetch error: \(error.userInfo)")
         }
     }
 
@@ -41,7 +54,7 @@ class SummaryViewController: UIViewController {
             let excerciseController = segue.destination as! ExcercisesController
             let cellIndex = tableView.indexPathForSelectedRow!
             excerciseController.managedContext = managedContext
-            excerciseController.workoutName = workouts[cellIndex.row]["name"] as! String
+            excerciseController.workoutName = workoutTypes[cellIndex.row]["name"] as! String
         }
     }
 }
@@ -49,12 +62,21 @@ class SummaryViewController: UIViewController {
 // Table View Delegate
 extension SummaryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return workouts.count
+        if isAllWorkouts {
+            return workouts.count
+        }
+        return workoutTypes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = workouts[indexPath.row]["name"] as? String
+        
+        if isAllWorkouts {
+            let workout = workouts[indexPath.row]
+            cell.textLabel?.text = "\(workout.name!): \(workout.startTime!)"
+        } else {
+            cell.textLabel?.text = workoutTypes[indexPath.row]["name"] as? String
+        }
         return cell
     }
 }
@@ -93,6 +115,14 @@ extension SummaryViewController: UITableViewDelegate {
         return nil
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if isAllWorkouts == false {
+            performSegue(withIdentifier: "ShowExcercises", sender: self)
+        } else {
+            performSegue(withIdentifier: "ShowWorkoutStats", sender: self)
+        }
+    }
+    
     func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath)
         cell?.contentView.backgroundColor = UIColor(colorLiteralRed: 100/255, green: 212/255, blue: 255/255, alpha: 1.0)
@@ -104,12 +134,23 @@ extension SummaryViewController: UITableViewDelegate {
     }
     
     func segmentControlValueChanged(segment: UISegmentedControl) {
+        
         if segment.selectedSegmentIndex == 0 {
             // Show only types of workouts created
+            isAllWorkouts = false
+            tableView.reloadData()
         }
         
         if segment.selectedSegmentIndex == 1 {
-            // Show All Workoutss
+            // Show All Workouts
+            isAllWorkouts = true
+            guard workouts.count == 0 else {
+                tableView.reloadData()
+                return
+            }
+            
+            fetchAllWorkouts()
+            tableView.reloadData()
         }
     }
 }
