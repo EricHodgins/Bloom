79af948035248283fc857c9d84444c078179b273
Eric Hodgins <hodgins.e@gmail.com>
2017-08-09 01:34:46 -0400

    feat: Made basic UI for LiveMapController.

--- Bloom/LiveMapViewController.swift
+++ Bloom/LiveMapViewController.swift
@@ -13,7 +13,6 @@ class LiveMapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
 
